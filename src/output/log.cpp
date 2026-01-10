8ad78bca5207c81284b4db96551cc32832f2938a
Max Tasker <mmttasker@gmail.com>
2026-01-10 15:36:18 +0000

    write log start time and use std::ios::app

--- src/output/log.cpp
+++ src/output/log.cpp
@@ -2,13 +2,19 @@
 
 #include <stdexcept>
 #include <fstream>
+#include <chrono>
+#include <format>
 
 void log::open_file(const char* filename) {
-	file_.open(filename, std::ios::out);
+	file_.open(filename, std::ios::out | std::ios::app);
 
 	if (!file_.is_open()) {
 		throw std::runtime_error("Failed to open log file");
 	}
+
+	// write start time
+	auto now = std::chrono::system_clock::now();
+	file_ << std::format("=== Log started at {:%Y-%m-%d %H:%M:%S} ===\n", std::chrono::floor<std::chrono::seconds>(now));
 }
 void log::shutdown() {
 	if (file_.is_open()) {
