b7fc35e0f3dc698b1375b0fe27550f0347196201
Max Tasker <mmttasker@gmail.com>
2026-01-10 16:00:16 +0000

    add class for ddlcd runtime errors

---
+++ include/error/ddlcd_runtime_error.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+enum class ddlcd_error {
+	OTHER,
+};
+
+class ddlcd_runtime_error : public std::exception {
+public:
+	ddlcd_runtime_error(ddlcd_error kind, const std::string& message)
+		: kind(kind), message(message) {
+	}
+
+	const char* what() const noexcept override {
+		return message.c_str();
+	}
+
+	ddlcd_error kind;
+	std::string message;
+};
