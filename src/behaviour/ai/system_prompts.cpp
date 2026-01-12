60e3b549ae01c788d741a33bf93dcca1cab513cb
Max Tasker <mmttasker@gmail.com>
2026-01-12 17:49:42 +0000

    fix prompt collection

--- src/behaviour/ai/system_prompts.cpp
+++ src/behaviour/ai/system_prompts.cpp
@@ -28,8 +28,9 @@ std::string system_prompts::get_prompt(ddlc_character character, const std::stri
 		else {
 			throw std::runtime_error("(Monika) Unknown prompt preset: " + mode);
 		}
-		
-		return bhv + "\n" + rules_monika;
+		bhv += std::string("\n") + "Monika expressions: " + monika_expressions;
+		bhv += std::string("\n") + "Monika poses: " + monika_poses;
+		break;
 	}
 	case ddlc_character::YURI: {
 		if (mode == "postgame" || mode == "default" || mode.empty()) {
@@ -47,8 +48,9 @@ std::string system_prompts::get_prompt(ddlc_character character, const std::stri
 		else {
 			throw std::runtime_error("(Yuri) Unknown prompt preset: " + mode);
 		}
-
-		return bhv + "\n" + rules_yuri;
+		bhv += std::string("\n") + "Yuri expressions: " + yuri_expressions;
+		bhv += std::string("\n") + "Yuri poses: " + yuri_poses;
+		break;
 	}
 	case ddlc_character::NATSUKI: {
 		if (mode == "postgame" || mode == "default" || mode.empty()) {
@@ -66,8 +68,9 @@ std::string system_prompts::get_prompt(ddlc_character character, const std::stri
 		else {
 			throw std::runtime_error("(Natsuki) Unknown prompt preset: " + mode);
 		}
-
-		return bhv + "\n" + rules_natsuki;
+		bhv += std::string("\n") + "Natsuki expressions: " + natsuki_expressions;
+		bhv += std::string("\n") + "Natsuki poses: " + natsuki_poses;
+		break;
 	}
 	case ddlc_character::SAYORI: {
 		if (mode == "postgame" || mode == "default" || mode.empty()) {
@@ -88,10 +91,13 @@ std::string system_prompts::get_prompt(ddlc_character character, const std::stri
 		else {
 			throw std::runtime_error("(Sayori) Unknown prompt preset: " + mode);
 		}
-
-		return bhv + "\n" + rules_sayori;
+		bhv += std::string("\n") + "Sayori expressions: " + sayori_expressions;
+		bhv += std::string("\n") + "Sayori poses: " + sayori_poses;
+		break;
 	}
 	default:
 		throw std::runtime_error("Unknown character for system prompt");
 	}
+
+	return bhv + "\n" + rules;
 }
